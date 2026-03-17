99e87a129f9adbae57b665f79e5a0adce19e539d
Shashvat Patel <shashvatpatel006@gmail.com>
2026-03-17 14:26:41 +0530

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,11 @@
 const admin = require('firebase-admin');
+require('dotenv').config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
